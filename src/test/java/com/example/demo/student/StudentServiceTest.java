432f66eab642a77d94efa5631bc6988d9f805885
Diana Eftaiha <diana.eftaiha@redeye.co>
2022-04-22 15:44:05 +0300

    add remove MockitoExtension

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,23 +1,29 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
     @Mock
     private StudentRepository studentRepository;
+    private AutoCloseable autoCloseable;
     private StudentService studentService;
 
     @BeforeEach
     void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
         studentService = new StudentService(studentRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
 
     @Test
